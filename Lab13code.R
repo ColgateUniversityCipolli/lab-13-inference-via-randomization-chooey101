b11a712bd44fb228b411f09b4b339d3c9d8772d8
Charlie Hooey <charleshooey@gmail.com>
2025-05-01 15:54:12 -0400

    3b completed

--- Lab13code.R
+++ Lab13code.R
@@ -107,8 +107,69 @@ resamples.null.diff <- replicate(R, {
 })
 
 #Bootstrapped p-vals
-p_closer_boot <- mean(
-  abs(resamples.null.closer) >= abs(t_closer)
-)
+p_closer_boot  <- mean(abs(resamples.null.closer)  >= abs(t_closer))
+p_further_boot <- mean(abs(resamples.null.further) >= abs(t_further))
+p_diff_boot    <- mean(abs(resamples.null.diff)    >= abs(t_diff))
 
-p_further_boot <- mean(abs())
+#Part 2c: Compute 5th percentile under shifted null dist
+
+t05_closer_boot  <- quantile(resamples.null.closer,  probs = 0.05)
+t05_further_boot <- quantile(resamples.null.further, probs = 0.05)
+t05_diff_boot    <- quantile(resamples.null.diff,    probs = 0.05)
+
+# Compute theoretical t critical values at α = 0.05 (left tail)
+t05_closer_theo  <- qt(0.05, df = n_closer - 1)
+t05_further_theo <- qt(0.05, df = n_further - 1)
+t05_diff_theo    <- qt(0.05, df = n_diff    - 1)
+
+#Part 2d: Compute confidence intervals
+probs <- c(0.025, 0.975)
+
+# 1. Closer data
+q_closer        <- quantile(resamples.null.closer,  probs = probs)
+ci_closer_boot  <- c(
+  mean(closer) - q_closer[2] * (s_closer / sqrt(n_closer)),
+  mean(closer) - q_closer[1] * (s_closer / sqrt(n_closer))
+)
+tci_closer      <- t.test(closer,  conf.level = 0.95)$conf.int
+
+# 2. Further data
+q_further        <- quantile(resamples.null.further, probs = probs)
+ci_further_boot  <- c(
+  mean(further) - q_further[2] * (s_further / sqrt(n_further)),
+  mean(further) - q_further[1] * (s_further / sqrt(n_further))
+)
+tci_further      <- t.test(further, conf.level = 0.95)$conf.int
+
+# 3. Difference data
+q_diff           <- quantile(resamples.null.diff,    probs = probs)
+ci_diff_boot     <- c(
+  mean(difference) - q_diff[2] * (s_diff / sqrt(n_diff)),
+  mean(difference) - q_diff[1] * (s_diff / sqrt(n_diff))
+)
+tci_diff         <- t.test(difference, conf.level = 0.95)$conf.int
+
+#Part 3: Randomization Testing
+#3a
+
+rand.t.closer <- replicate(R, {
+  signs <- sample(c(-1,1), n.closer, replace = TRUE)
+  mean(signs * closer.null)  / (s.closer   / sqrt(n.closer))
+})
+
+rand.t.further <- replicate(R, {
+  signs <- sample(c(-1,1), n.further, replace = TRUE)
+  mean(signs * further.null) / (sd.further / sqrt(n.further))
+})
+
+rand.t.diff <- replicate(R, {
+  signs <- sample(c(-1,1), n.diff, replace = TRUE)
+  mean(signs * diff.null)    / (sd.diff    / sqrt(n.diff))
+})
+
+#3b: Compute Randomization P values
+p_closer_rand  <- mean(abs(rand.t.closer)  >= abs(t_closer))
+p_further_rand <- mean(abs(rand.t.further) >= abs(t_further))
+p_diff_rand    <- mean(abs(rand.t.diff)    >= abs(t_diff))
+
+#3c: Compute confidence intervals
